5f361c824e9dcdeba62b777a7a076a35f0aead4e
gil-x <gino.ladowitch@gmail.com>
2021-07-15 12:20:44 +0200

    Added Homelink as icon & Instagram social link.

--- footer.php
+++ footer.php
@@ -3,7 +3,9 @@
 		<a href="#" rel="nofollow">Mentions légales</a> | <a href="contact" >contact</a> | site réalisé par <a href="https://www.happy-dev.fr/" >Happy Dev</a>
 	</div>
 	<div class="social">
-		<a href="#" rel="nofollow" >Linkedin</a><a href="" >Twitter</a>
+		<a target="_blank" href="https://www.linkedin.com/in/ahlam-arnaout" >Linkedin</a>
+		<a target="_blank" href="https://twitter.com/halimatoune" >Twitter</a>
+		<a target="_blank" href="https://www.instagram.com/dr.ahlam.arnaout/" >Instagram</a>
 	</div> 
 </footer>
 	<?php wp_footer(); ?>
